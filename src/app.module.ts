61d933a33c2d01c7211b825babcf980d6ed2b8d5
Julio Saito <saitodisse@gmail.com>
2023-01-19 00:59:05 -0300

    add bot example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
+import { DiscordModule } from '@discord-nestjs/core'
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { GatewayIntentBits } from 'discord.js'
+
+import { BotGateway } from './bot/bot.gateway'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    DiscordModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        token: configService.get('TOKEN'),
+        discordClientOptions: {
+          intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            // You must allow message content for your application in discord developers
+            // https://support-dev.discord.com/hc/en-us/articles/4404772028055
+            GatewayIntentBits.MessageContent,
+          ],
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [BotGateway],
 })
 export class AppModule {}
---
+++ src/bot/bot.gateway.ts
@@ -0,0 +1,30 @@
+import { InjectDiscordClient, On, Once, UseGuards, UsePipes } from '@discord-nestjs/core'
+import { Injectable, Logger } from '@nestjs/common'
+import { Client, Message } from 'discord.js'
+
+import { MessageFromUserGuard } from './guards/message-from-user.guard'
+import { MessageToUpperPipe } from './pipes/message-to-upper.pipe'
+
+@Injectable()
+export class BotGateway {
+  private readonly logger = new Logger(BotGateway.name)
+
+  constructor(
+    @InjectDiscordClient()
+    private readonly client: Client
+  ) {}
+
+  @Once('ready')
+  onReady() {
+    this.logger.log(`Bot ${this.client.user.tag} was started!`)
+  }
+
+  @On('messageCreate')
+  @UseGuards(MessageFromUserGuard)
+  @UsePipes(MessageToUpperPipe)
+  async onMessage(message: Message): Promise<void> {
+    this.logger.log(`Incoming message: ${message.content}`)
+
+    await message.reply('Message processed successfully')
+  }
+}
